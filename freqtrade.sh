4ef26d57126d952812999f11a53247dbe29b45fe
Xaemiphor <xaemiphor@users.noreply.github.com>
2025-01-10 22:30:50 -0800

    ci: testing alternative daily cron

--- freqtrade.sh
+++ freqtrade.sh
@@ -2,6 +2,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 [ -z "${EXCHANGE:-}" ] && echo "ERROR: 'EXCHANGE' env var not defined" && exit 1
 [ -z "${STAKE:-}" ] && echo "ERROR: 'STAKE' env var not defined" && exit 1
+docker pull -q freqtradeorg/freqtrade:stable >/dev/null 2>&1
 docker run \
 	--rm \
 	-v "${SCRIPT_DIR}/docker:/docker" \
