6e043e29e6ed58c72104ec98e57f62f68e1dc376
Xaemiphor <xaemiphor@users.noreply.github.com>
2025-01-16 10:35:49 -0800

    feat: restructure to support non-spot in the future

--- freqtrade.sh
+++ freqtrade.sh
@@ -2,6 +2,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 [ -z "${EXCHANGE:-}" ] && echo "ERROR: 'EXCHANGE' env var not defined" && exit 1
 [ -z "${STAKE:-}" ] && echo "ERROR: 'STAKE' env var not defined" && exit 1
+[ -z "${TRADINGMODE:-}" ] && echo "ERROR: 'TRADINGMODE' env var not defined" && exit 1
 docker pull -q freqtradeorg/freqtrade:stable >/dev/null 2>&1
 docker run \
 	--rm \
@@ -10,4 +11,7 @@ docker run \
 	-v "${SCRIPT_DIR}/user_data:/freqtrade/user_data" \
 	--entrypoint "/docker/entrypoint.sh" \
 	freqtradeorg/freqtrade:stable "$@" \
-	-c /configs/common.json -c /configs/exchange-${EXCHANGE}.json -c /configs/stake-${STAKE}.json
+	-c /configs/common.json \
+	-c /configs/exchange-${EXCHANGE}.json \
+	-c /configs/stake-${STAKE}.json \
+	-c /configs/tradingmode-${TRADINGMODE}.json
